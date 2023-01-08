6ed8fff82b08dab98ee38bf1a16afc6673a1ec6b
Esemwy <github.5.smy@neverbox.com>
2023-01-08 16:41:27 -0500

    more boilerplate to stardrifter

--- module/stardrifter.mjs
+++ module/stardrifter.mjs
@@ -6,7 +6,7 @@ import { StardrifterActorSheet } from "./sheets/actor-sheet.mjs";
 import { StardrifterItemSheet } from "./sheets/item-sheet.mjs";
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
-import { BOILERPLATE } from "./helpers/config.mjs";
+import { STARDRIFTER } from "./helpers/config.mjs";
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
@@ -23,7 +23,7 @@ Hooks.once('init', async function() {
   };
 
   // Add custom constants for configuration.
-  CONFIG.BOILERPLATE = BOILERPLATE;
+  CONFIG.STARDRIFTER = STARDRIFTER;
 
   /**
    * Set an initiative formula for the system
